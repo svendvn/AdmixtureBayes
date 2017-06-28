ae32ebc6a260265cddfe1531029aaff751824e40
svendvn <svend1991@msn.com>
2017-06-28 11:55:53 -0700

    a catch-all method for initialising posterior

--- AdmixtureBayes/src/posterior.py
+++ AdmixtureBayes/src/posterior.py
@@ -2,10 +2,79 @@ from prior import prior
 from likelihood import likelihood, n_mark
 from scipy.stats import norm, multivariate_normal
 from math import log
+from generate_prior_trees import generate_phylogeny
+from tree_statistics import identifier_to_tree_clean
+from Rtree_operations import get_number_of_leaves, get_number_of_admixes, remove_outgroup, simple_reorder_the_leaves_after_removal_of_s1
+from tree_to_data import reduce_covariance
+from Rtree_to_covariance_matrix import make_covariance
 
 
-
-def initialize_posterior(emp_cov, M=None, use_skewed_distr=False, p=0.5, rescale=False):
+def initialize_posterior(emp_cov=None, 
+                         true_tree=None, 
+                         M=None, 
+                         use_skewed_distr=False, 
+                         p=0.5, 
+                         rescale=False, 
+                         model_choice=['empirical covariance',
+                                       'true tree covariance',
+                                       'wishart on true tree covariance',
+                                       'empirical covariance on true tree',
+                                       'no likelihood'],
+                         simulate_true_tree=False,
+                         true_tree_no_leaves=None,
+                         true_tree_no_admixes=None,
+                         nodes=None,
+                         simulate_true_tree_with_skewed_prior=False,
+                         reduce_cov=None,
+                         reduce_true_tree=False):
+    
+    if not isinstance(model_choice, basestring):
+        model_choice=model_choice[0]
+        
+    if model_choice == 'no likelihood':
+        return initialize_prior_as_posterior(), {}
+        
+    if (model_choice == 'true tree covariance' or 
+        model_choice == 'wishart on true tree covariance' or
+        model_choice == 'empirical covariance on true tree'):
+        
+        if simulate_true_tree:
+            true_tree= generate_phylogeny(true_tree_no_leaves,
+                               true_tree_no_admixes,
+                               nodes,
+                               simulate_true_tree_with_skewed_prior)
+            
+        elif isinstance(true_tree, basestring):
+            if ';' in true_tree: #this means that the true tree is a s_tree
+                true_tree_s=true_tree
+                true_tree=identifier_to_tree_clean(true_tree_s)
+            else:
+                with open(true_tree, 'r') as f:
+                    true_tree_s=f.readline().rstrip()
+                true_tree=identifier_to_tree_clean(true_tree_s)
+        
+        if reduce
+        if reduce_true_tree is not None:
+            true_tree=Rtree_operations.remove_outgroup(true_tree, reduce_true_tree)
+            if reduce_true_tree=='s1' or reduce_true_tree==0:
+                
+        true_tree=Rtree_operations.simple_reorder_the_leaves_after_removal_of_s1(true_tree)
+                
+        
+        no_leaves = get_number_of_leaves(true_tree)
+        no_admixes = get_number_of_admixes(true_tree)
+        
+        
+        
+        cov=make_covariance(true_tree)
+        
+        if reduce_cov is not None:
+             
+        
+        if emp_cov is not None:
+            if isinstance(emp_cov, basestring):
+                
+    
     if M is None:
         M=n_mark(emp_cov)
     if rescale:
