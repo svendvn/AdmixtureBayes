cd05e0e8633611cfad9450f9bb1b5d08945079a2
svendvn <svend1991@msn.com>
2017-09-07 15:47:48 +0200

    updating another flad

--- AdmixtureBayes/src/call_AdmixtureBayes.py
+++ AdmixtureBayes/src/call_AdmixtureBayes.py
@@ -32,6 +32,7 @@ parser.add_argument('--covariance_pipeline', nargs='+', type=int, default=[2,3,4
 parser.add_argument('--outgroup_name', type=str, default='out', help='the name of the outgroup that should be added to a simulated dataset.')
 parser.add_argument('--reduce_node', type=str, default='out', help='the name of the population that should be made root.')
 parser.add_argument('--nodes', type=str, nargs='+', default=[''], help= 'list of nodes of the populations or the filename of a file where the first line contains all population names. If unspecified the first line of the input_file will be used. If no input file is found, there will be used standard s1,..,sn.')
+parser.add_argument('--wishart_noise', action='store_true', default=False)
 
 #prior options
 parser.add_argument('--p', type=float, default=0.5, help= 'the geometrical parameter in the prior. The formula is p**x(1-p)')
@@ -142,7 +143,8 @@ covariance=get_covariance(options.covariance_pipeline,
                           scale_tree_factor=options.scale_tree_factor,
                           prefix=prefix,
                           t_adjust_tree=options.time_adjusted_tree,
-                          sadmix=options.sadmix_tree)
+                          sadmix=options.sadmix_tree,
+                          add_wishart_noise_to_covariance=options.wishart_noise)
 
 no_pops=len(reduced_nodes)
 
