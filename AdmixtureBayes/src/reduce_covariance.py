88aa2ccb633cc84578436b74f5fab9803543b445
svendvn <svend1991@msn.com>
2018-02-10 00:32:04 +0100

    Adding Areduce

--- AdmixtureBayes/src/reduce_covariance.py
+++ AdmixtureBayes/src/reduce_covariance.py
@@ -1,5 +1,17 @@
-from numpy import insert, identity
+from numpy import insert, identity,ones
 
 def reduce_covariance(covmat, subtracted_population_index):
     reducer=insert(identity(covmat.shape[0]-1), subtracted_population_index, -1, axis=1)
+    #print reducer
     return reducer.dot(covmat).dot(reducer.T)
+
+def Areduce(mat):
+    A=identity(mat.shape[0])-1.0/mat.shape[0]*ones((mat.shape))
+    return A.dot(mat).dot(A)
+
+
+if __name__=='__main__':
+    
+    
+    print reduce_covariance(identity(4),0
+                            )
