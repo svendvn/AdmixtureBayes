b3bc8bde406627819b7b1054972dfbe5e15341f6
svendvn <svend1991@msn.com>
2018-02-20 17:43:53 +0100

    different df estimators

---
+++ AdmixtureBayes/src/df_estimators.py
@@ -0,0 +1,46 @@
+from scipy.stats import wishart
+import numpy as np
+from scipy.optimize import minimize
+
+def likelihood_mean_based(sample_of_matrices):
+    
+    mean_wishart=  np.mean(sample_of_matrices, axis=0)
+    print [np.linalg.det(s) for s in sample_of_matrices]
+    print 'mean_wishart', mean_wishart
+    print np.linalg.det(mean_wishart)
+    print np.linalg.matrix_rank(mean_wishart)
+    r=mean_wishart.shape[0]
+    
+    def joint_density(df_l, verbose=True):
+        df=df_l[0]
+        val=-sum((wishart.logpdf(x, df=df, scale=mean_wishart/df)  for x in sample_of_matrices))
+        if verbose:
+            print df, ':', val
+        return val
+    
+    return minimize(joint_density, r, bounds=[(r,None)]).x[0]
+
+
+def variance_mean_based(sample_of_matrices):
+    mean_wishart=  np.mean(sample_of_matrices, axis=0)
+    var_wishart= np.var(sample_of_matrices, axis=0)
+    r=mean_wishart.shape[0]
+    var_rom_mean_wishart=(np.square(mean_wishart)+np.diag(mean_wishart)).T+np.diag(mean_wishart)
+    def penalty_function(df_l):
+        df=df_l[0]
+        val=np.linalg.norm(var_wishart-var_rom_mean_wishart/df)
+        print df, '::', val
+        return val
+    
+    return minimize(penalty_function, r, bounds=[(r,None)]).x[0]
+    
+
+
+if __name__=='__main__':
+    Sigma=np.identity(4)
+    df=1043
+    xs=[wishart.rvs(scale=Sigma/df, df=df) for _ in xrange(100)]
+    
+    print variance_mean_based(xs)
+    print likelihood_mean_based(xs)
+    
