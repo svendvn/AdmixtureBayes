30fb4908cc099e83894a21fbe49ce85094aa69e6
Svend V. Nielsen <svn@cs.au.dk>
2018-10-18 15:56:26 +0200

    making cython optional

--- setup.py
+++ setup.py
@@ -1,30 +1,43 @@
 from setuptools import setup, find_packages, Extension
-from Cython.Build import cythonize
-import numpy as np
 import os
 
-# extra compiler flags to make it highly optimized and fast. `-march=native` is
-# a gcc-specific flag, I think.
-compile_args = ['-march=native', '-O3']
+try:
+    from Cython.Build import cythonize
+    import numpy as np
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
 
-extensions = [
-            Extension(
-                # oddly, the module name (here, _interp) has to be the same as
-                # the basename (ie prefix) of the full filename.
-                '_covariance_matrix',
-                [ os.path.join('AdmixtureBayes','src', '_covariance_matrix.pyx')],
-                include_dirs=[np.get_include()],
-                extra_compile_args = compile_args)
-            ]
-            
+if use_cython:
+    # extra compiler flags to make it highly optimized and fast. `-march=native` is
+    # a gcc-specific flag, I think.
+    compile_args = ['-march=native', '-O3']
 
+    extensions = cythonize([
+                Extension(
+                    # oddly, the module name (here, _interp) has to be the same as
+                    # the basename (ie prefix) of the full filename.
+                    '_covariance_matrix',
+                    [ os.path.join('admixturebayes', '_covariance_matrix.pyx')],
+                    include_dirs=[np.get_include()],
+                    extra_compile_args = compile_args)
+                ]
+            )
+else:
+    extensions = [
+        Extension(
+            '_covariance_matrix', 
+            [os.path.join('admixturebayes',  '_covariance_matrix.o')], 
+        )
+    ]
 
 setup(
         name='AdmixtureBayes',
         version='0.1',
         license='Creative Commons Attribution-Noncommercial-Share Alike license',
         long_description=open('README.md').read(),
-        ext_modules = cythonize(extensions), 
+        ext_modules=extensions, 
         zip_safe=False, 
         
         install_requires=[
@@ -36,12 +49,14 @@ setup(
             "pygraphviz"
         ],
         
-        package_dir = {'': 'AdmixtureBayes'}, 
-        packages=['src'],
+        packages=find_packages(),
         
         entry_points={'console_scripts': [
-            'AdmixtureBayes = src.__main__:main'
+            'AdmixtureBayes = admixturebayes.__main__:main'
         ]},
+        
+        package_data={b'': ['admixturebayes/*.R']},
+        include_package_data=True,
         )
         
 
