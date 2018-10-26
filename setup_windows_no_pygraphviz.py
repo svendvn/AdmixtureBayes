42fffa9b2b88ee3463666f875e64a29419cc6632
svendvn <svend1991@msn.com>
2018-10-27 00:38:45 +0200

    version without pygraphciz

---
+++ setup_windows_no_pygraphviz.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='AdmixtureBayes',
+        version='0.1',
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        long_description=open('README.md').read(),
+        zip_safe=False, 
+        
+        install_requires=[
+            "numpy>=1.11.0",
+            "scipy>=0.17.0", 
+            "matplotlib", 
+            "pandas", 
+            "pathos", 
+        ],
+        
+        packages=find_packages(),
+        
+        entry_points={'console_scripts': [
+            'AdmixtureBayes = admixturebayes.__main__:main'
+        ]},
+        
+        package_data={b'': ['admixturebayes/*.R']},
+        include_package_data=True,
+        )
+        
+
