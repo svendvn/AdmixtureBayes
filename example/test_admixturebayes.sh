937c6db69e819eceb34906e6c56f2747f42949f6
Svend V. Nielsen <svn@cs.au.dk>
2018-10-31 23:14:19 +0100

    put all workshop cases in test file

--- example/test_admixturebayes.sh
+++ example/test_admixturebayes.sh
@@ -1,3 +1,5 @@
 AdmixtureBayes run --input_file big_allele_counts.txt --outgroup out
-AdmixtureBayes posterior --input_file result_mc3.csv --covariance_matrix_file covariance_and_multiplier.txt --outgroup_name out
-AdmixtureBayes plot --posterior_distribution_file posterior_distributions.csv --plot consensus_trees --write_ranking_to_file rankings.txt
+AdmixtureBayes posterior --input_file result_mc3.csv --covariance_matrix_file covariance_and_multiplier.txt --outgroup_name out 
+AdmixtureBayes plot --posterior_distribution_file posteriors_small.csv --plot consensus_trees --write_ranking_to_file rankings.txt
+#AdmixtureBayes posterior --input_file result_mc3.csv --covariance_matrix_file covariance_and_multiplier.txt --outgroup_name out --subnodes s1 s2 s3 --result_file posteriors_small.csv
+#AdmixtureBayes qp --posterior_distribution_file posterior_distributions.csv 
