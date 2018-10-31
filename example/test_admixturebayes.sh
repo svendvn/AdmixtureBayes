f03f1722fc2adac47aff014d9a464f950f7d1e53
Svend V. Nielsen <svn@cs.au.dk>
2018-10-31 22:20:50 +0100

    removing prefix as standard

--- example/test_admixturebayes.sh
+++ example/test_admixturebayes.sh
@@ -1,3 +1,3 @@
-AdmixtureBayes run --input_file big_allele_counts.txt --outgroup out --prefix svend
+AdmixtureBayes run --input_file big_allele_counts.txt --outgroup out 
 AdmixtureBayes posterior --input_file result_mc3.csv --covariance_matrix_file covariance_and_multiplier.txt 
 AdmixtureBayes plot --posterior_distribution_file posterior_distributions.csv --plot consensus_trees --write_ranking_to_file rankings.txt
